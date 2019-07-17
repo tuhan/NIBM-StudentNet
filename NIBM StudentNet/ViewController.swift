2923b573eb7ab0f969e376d18c197ea0fcf7f97b
Tuhan Sapumanage <tuhan.binuja@gmail.com>
2019-07-17 15:12:19 +0530

    First Commit

---
+++ NIBM StudentNet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NIBM StudentNet
+//
+//  Created by Tuhan Sapumanage on 7/17/19.
+//  Copyright © 2019 Tuhan Sapumanage. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
