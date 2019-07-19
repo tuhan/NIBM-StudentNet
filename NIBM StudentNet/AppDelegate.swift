d4a27e3c3769520757c3aeecc290bd08c0bde94c
Sandeepa Manawadu <msmanawadu@gmail.com>
2019-07-19 22:28:10 +0530

    Bug Fix - Storyboard UI constrants resetted

--- NIBM StudentNet/AppDelegate.swift
+++ NIBM StudentNet/AppDelegate.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-
 import Firebase
 
 @UIApplicationMain
@@ -18,9 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
         FirebaseApp.configure()
-        
         return true
     }
 
