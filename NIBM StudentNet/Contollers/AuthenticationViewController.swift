9d4327b09cefda9fcf1fb352990f60fbf69aa781
Sandeepa Manawadu <msmanawadu@gmail.com>
2019-07-19 15:09:21 +0530

    Done with basic UI for Authentication storyboard

---
+++ NIBM StudentNet/Contollers/AuthenticationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AuthenticationViewController.swift
+//  NIBM StudentNet
+//
+//  Created by Sandeepa Manawadu on 2019/07/19.
+//  Copyright © 2019 Tuhan Sapumanage. All rights reserved.
+//
+
+import UIKit
+
+class AuthenticationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
